4c279b8917c772117837c6743c9a7aaa8e8f8a25
Lcj-space <1367546524@qq.com>
2016-12-07 21:59:30 +0800

    九九乘法第一种表示形式

--- afterclass/04-jiujiu-for1.php
+++ afterclass/04-jiujiu-for1.php
@@ -1,11 +1,15 @@
 <?php
 for ($i = 1; $i <= 9; $i++) {
     for ($j = 1; $j <= $i; $j++) {
-        echo "<li style='text-align: center;line-height: 30px;display: inline; 
-               border: 1px solid black;
-               width: 50px;height: 30px;list-style: none'>";
+//        echo "<li style='text-align: center;line-height: 30px;display: inline;
+//               border: 1px solid black;
+//               width: 50px;height: 30px;list-style: none'>";
+        echo "<ul style='list-style: none'>";
+        echo "<li style='float: left ;border: 1px solid black;margin: 1px;text-align: center;'>";
         echo $i . '*' . $j . '=' . $i * $j;
         echo "</li>";
+        echo "</ul>";
+        //        echo "</li>";
     }
     echo "</br>";
 }
