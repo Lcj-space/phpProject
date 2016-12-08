d766bf0a569a41338f0967270cd111cf60771266
Lcj-space <1367546524@qq.com>
2016-12-08 23:35:43 +0800

    计算器的编辑

--- afterclass/06-jisunqi.php
+++ afterclass/06-jisunqi.php
@@ -2,7 +2,7 @@
 if (isset($_POST['sub'])) {
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
-    echo $num1;
+//    echo $num1;
     $ysf = $_POST['ysf'];
     $sum = 0;
     switch ($ysf) {
@@ -20,9 +20,10 @@ if (isset($_POST['sub'])) {
         case '/':
             $sum = $num1 / $num2;
             break;
-
+        default:
+            echo "运算符非法";
     }
-    echo $sum;
+//    echo $sum;
 }
 ?>
 <html>
@@ -36,27 +37,63 @@ if (isset($_POST['sub'])) {
 
         #container {
             margin: 100px;
+
+
         }
+
+
+
     </style>
 </head>
 <body>
 <div id="container">
-    <form action="06-jisunqi.php" method="post">
-        <input type="text" name="num1">
-        <select name="ysf">
-            <option value="+">+</option>
+    <table id="content">
+        <form action="06-jisunqi.php" method="post">
+            <tr>
+                <td><input type="text" name="num1" value=<?php echo $num1 ?>></td>
 
-            <option value="-">-</option>
+                <td><select name="ysf">
+                        <option value="+"<?php if ($ysf == '+') {
+                            echo 'selected';
+                        } ?>>+
+                        </option>
 
-            <option value="X">*</option>
+                        <option value="-"<?php if ($ysf == '-') {
+                            echo 'selected';
+                        } ?>>-
+                        </option>
 
-            <option value="/">/</option>
+                        <option value="X"<?php if ($ysf == '*') {
+                            echo 'selected';
+                        } ?>>*
+                        </option>
 
-        </select>
-        <input type="text" name="num2">
-        <input type="button" name="sub" value="计算">
-    </form>
-    <input type="text" value="计算结果">
+                        <option value="/"<?php if ($ysf == '/') {
+                            echo 'selected';
+                        } ?>>/
+                        </option>
+                    </select>
+                </td>
+                <td><input type="text" name="num2" value=<?php echo $num2 ?>></td>
+                <td><input type="submit" name="sub" value="计算"></td>
+            </tr>
+            <tr>
+                <input class="result" type="text" value="结果为：<?php
+                if($num1=='a'||$num1=='%'){
+                    echo $num1."第一个不是数字";
+                }
+                else if($num2=='a'||$num2=='%'){
+                    echo $num2."第二个不是数字";
+                }else
+                echo $num1.'*'.$num2.'='.$sum ;
+
+                ?>">
+            </tr>
+        </form>
+
+
+    </table>
+
 </div>
 </body>
 </html>
